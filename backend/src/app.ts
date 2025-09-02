fed5f903f6ab4d23a1265631887809c000f2d8df
Anurag Rajput <code.annu@gmail.com>
2025-09-02 11:41:27 +0530

    add db config file

---
+++ backend/src/app.ts
@@ -0,0 +1,28 @@
+import express from "express";
+import { createServer } from "http";
+import { connectDB } from "./config/db";
+
+import cors from "cors";
+
+const app = express();
+const server = createServer(app);
+
+const corsOptions = {
+  origin: "http://localhost:5173",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true, // if you're sending cookies or auth headers
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// app.use("/api/projects", validateToken, projectRouter);
+
+const PORT = 3000;
+connectDB().then(() => {
+  server.listen(PORT, () => {
+    console.log(`Server is listening at: http://localhost:${PORT}/api`);
+  });
+});
